9c8bddc95a8131470f23eadb44fe1b6a0480542c
Artur Szulc <s24260@szuflandia.pjwstk.edu.pl>
2022-04-11 21:36:54 +0200

    Dodanie pamietanika z zjazdu trzeciego, poprawa pamietnika, dodanie zadania domowego z zjazdu 2 oraz poprawa zadania 5b, dodanie zadań domowych z zjazdu 3

--- s24260-ArturSzulc/zajecia2/zadanie5b.sh
+++ s24260-ArturSzulc/zajecia2/zadanie5b.sh
@@ -4,8 +4,4 @@ name=$1
 echo "pierwszy: ${name:0:1}"
 echo "ostatni: ${name: -1}"
 
-if [ ${name} = "SOP" ]; then
-	echo -e "\e[32mSOP\e[0m"
-else 
-	echo $name
-fi
+echo $name | sed s/SOP/`echo -e "\e[32mSOP\e[0m"`/g
---
+++ s24260-ArturSzulc/zajecia2/zadanie_domowe.sh
@@ -0,0 +1,3 @@
+#!/bin/bash/
+
+sed -e s/artur/`echo -e "\e[31martur\e[0m"`/g $1
