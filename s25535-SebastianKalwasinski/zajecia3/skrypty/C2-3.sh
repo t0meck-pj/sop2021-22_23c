7704223bd1a99ba64b147f54658eb25f61be6972
Sebastian Kalwasiński <s25535@szuflandia.pjwstk.edu.pl>
2022-04-30 13:15:53 +0200

    Aktualizacja

--- s25535-SebastianKalwasinski/zajecia3/skrypty/C2-3.sh
+++ s25535-SebastianKalwasinski/zajecia3/skrypty/C2-3.sh
@@ -4,8 +4,16 @@ green=$(echo -e "\e[32m")
 lblue=$(echo -e "\e[94m")
 k0=$(echo -e "\e[0m")
 
-Pliki=$(find . -maxdepth 1 -type f | sed s/\\.\\///g)
-Katalogi=$(find . -maxdepth 1 ! -path . -type d | sed s/\\.\\///g)
+#Pliki=$(find . -maxdepth 1 -type f | sed s/\\.\\///g)
+#Katalogi=$(find . -maxdepth 1 ! -path . -type d | sed s/\\.\\///g)
+#${zmienna/"To"/"NaTo"/} - Jednorazowo 
+#${zmienna//"To"/"NaTo"/} - Globalnie
+
+Pli=$(find . -maxdepth 1 -type f)
+Kat=$(find . -maxdepth 1 ! -path . -type d)
+
+Pliki=${Pli//"./"/}
+Katalogi=${Kat//"./"/}
 
 for i in ${Pliki}; do
 	echo "Paczaj na plika: ${lblue}${i}${k0}"
