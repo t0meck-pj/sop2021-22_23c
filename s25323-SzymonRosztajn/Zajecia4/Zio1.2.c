6a7a2705348f9b3b8e8cbe30ef2626902e2717da
s25323-pj <92204632+s25323-pj@users.noreply.github.com>
2022-06-22 17:47:43 +0200

    Update Zio1.2.c

--- s25323-SzymonRosztajn/Zajecia4/Zio1.2.c
+++ s25323-SzymonRosztajn/Zajecia4/Zio1.2.c
@@ -1,1 +1,31 @@
-#
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    int index;
+    int index2;
+	int size;
+	int* numbers;
+	char* file_name = argv[1];
+    FILE* fp;
+	fp=fopen(file_name,"r");
+
+	if(fp){
+		fscanf(fp, "%d", &size);
+		numbers=malloc(size);
+
+		for(index = 0; index < size; index++) {
+			fscanf(fp, "%d", &numbers[index]);
+		}
+		for(index2 = size - 1; index2 >= 0; index2--) {
+			printf("%d ", numbers[index2]);
+		}
+        printf("\n");
+        free(numbers);
+        fclose(fp);
+	}else{
+		return 2;
+	}
+
+	return 0;
+}
