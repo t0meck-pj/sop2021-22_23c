8b1b014d2bb6a05562f91bfc549fbe6c1e64261f
s25670-pj <92204700+s25670-pj@users.noreply.github.com>
2022-06-22 19:47:16 +0200

    Update c3z2.sh

--- s25670-MateuszLech/zajecia3/c3z2.sh
+++ s25670-MateuszLech/zajecia3/c3z2.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 fib() {
-    declare wynik;
-    if [ $1 -le 1 ]; then
-        echo $1
+    if [[ $1 -le 1 ]]; then
+        return "$1"
     else
-        pierwszy=$(fib $(( $1 - 1 )));
-        drugi=$(fib $(( $1 - 2 )));
-        wynik=$(( $pierwszy + $drugi ));
-        echo $wynik;
+        fib $(($1-1))
+        local wynik=$?
+        fib $(($1-2))
+        return $((wynik+$?))
     fi
-    return $wynik
 }
 
 fib 20
