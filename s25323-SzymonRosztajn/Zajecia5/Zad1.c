726fdf06e53b6a50d561c8affd057a9620ced6a6
s25323-pj <92204632+s25323-pj@users.noreply.github.com>
2022-06-23 11:23:23 +0200

    Update Zad1.c

--- s25323-SzymonRosztajn/Zajecia5/Zad1.c
+++ s25323-SzymonRosztajn/Zajecia5/Zad1.c
@@ -1,1 +1,13 @@
-#
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+	int x;
+
+	printf("PID: %d\n", getpid());
+	printf("PPID: %d\n", getppid());
+	scanf("%d", &x);
+
+	return 0;
+}
