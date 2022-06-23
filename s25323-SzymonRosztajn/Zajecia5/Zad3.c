06ee5bf2a3d14bce752b49bb82988edd2a5e387d
s25323-pj <92204632+s25323-pj@users.noreply.github.com>
2022-06-23 21:01:15 +0200

    Update Zad3.c

--- s25323-SzymonRosztajn/Zajecia5/Zad3.c
+++ s25323-SzymonRosztajn/Zajecia5/Zad3.c
@@ -1,1 +1,30 @@
-#
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[])
+{
+        int num, i, result = 0, p = fork();
+        sscanf (argv[1],"%d",&num);
+
+        if (p > 0) 
+        {
+                waitpid(0, 0, 0);
+                for (i = 0; i <= num; i++) 
+                {
+                        result += i;
+                }
+
+                printf("Result: %d \n", result);
+        } 
+        else 
+        {
+                for (i = 0; i <= num; i++) 
+                {
+                        if (i % 2 == 1) printf("Number: %d \n", i);
+                }
+        }
+
+        return 0;
+}
