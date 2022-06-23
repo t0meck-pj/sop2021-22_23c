e1f7f9f905a8955c5002d200f735525316564ad4
s25323-pj <92204632+s25323-pj@users.noreply.github.com>
2022-06-23 21:36:49 +0200

    Update Zad4.c

--- s25323-SzymonRosztajn/Zajecia5/Zad4.c
+++ s25323-SzymonRosztajn/Zajecia5/Zad4.c
@@ -1,1 +1,33 @@
-#
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int next_child(int x, int level);
+
+int main()
+{
+    next_child (0,100);
+    
+        return 0;
+}
+
+int next_child(int i, int j) {
+  if (i <= j) 
+  {
+    printf("Poziom %d, PID: %d, PPID: %d \n", i, getpid(), getppid());
+      pid_t pid = fork();
+
+      if (pid == -1) 
+      {
+        return 1;
+      } 
+      else if (pid == 0) 
+      {
+        sleep(1);
+        return next_child(i + 1, j);
+      }
+
+  }
+    return wait(0);
+}
